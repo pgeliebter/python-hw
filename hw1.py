34e892078126ec8cade166de89e5998b66e0c4bb
Pinny Geliebter <39172757+pgeliebter@users.noreply.github.com>
2022-09-19 23:09:53 -0400

    updated

--- hw1.py
+++ hw1.py
@@ -1,24 +1,29 @@
-import random
+import random as rd
 
 # problem 1
+print("Problem 1: ")
 for i in range(1, 10, 2):
     print(i)
 
 # problem 2
+print("\nProblem 2: ")
 for i in range(10, 0, -1):
     print(i)
 
 # problem 3
+print("\nProblem 3: ")
 i = 10
 while (i > 0):
     print(i)
     i -= 1
 
 # problem 4
+print("\nProblem 4: ")
 for i in range(0, 10):
-    print(random())
+    print(rd.randint(1, 100))
 
 # extra credit
+print("\nExtra Credit:")
 barcode = 98201
 output = ''
 for i in range(len(str(barcode))):
