584b14b70422fdbac42d36503f738b9317ff28b4
Pinny Geliebter <39172757+pgeliebter@users.noreply.github.com>
2022-09-22 21:55:12 -0400

    all problems enabled

--- hw3.py
+++ hw3.py
@@ -1,43 +1,42 @@
 import csv
 
-# print("Problem 1: ")
+print("Problem 1: ")
 
 
-# def meanCalc(file):
-#     openedFile = open(file, 'r')
-#     csvReader = csv.reader(openedFile, delimiter=',')
-#     ageIndex = 2
-#     csvReader.__next__()
-#     mean = [0, 0]
-#     for row in csvReader:
-#         mean[0] += int(row[ageIndex])
-#         mean[1] += 1
-#     return (mean[0]/mean[1])
+def meanCalc(file):
+    openedFile = open(file, 'r')
+    csvReader = csv.reader(openedFile, delimiter=',')
+    ageIndex = 2
+    # call next row to skip header
+    csvReader.__next__()
+    mean = [0, 0]
+    for row in csvReader:
+        mean[0] += int(row[ageIndex])
+        mean[1] += 1
+    return (mean[0]/mean[1])
 
 
-# # calling and printing the output of function
-# print(meanCalc('birthwt.csv'))
+# calling and printing the output of function
+print(meanCalc('birthwt.csv'))
 
-# print("\nProblem 2: ")
-
-
-# def meanCalc(file, colIndex):
-#     openedFile = open(file, 'r')
-#     csvReader = csv.reader(openedFile, delimiter=',')
-#     csvReader.__next__()
-
-#     mean = [0, 0]
-#     for row in csvReader:
-#         mean[0] += int(row[colIndex])
-#         mean[1] += 1
-#     return (mean[0]/mean[1])
+print("\nProblem 2: ")
 
 
-# # calling and printing the output of function
-# print(meanCalc('birthwt.csv', 3))
+def meanCalc(file, colIndex):
+    openedFile = open(file, 'r')
+    csvReader = csv.reader(openedFile, delimiter=',')
+    # call next row to skip header
+    csvReader.__next__()
+    mean = [0, 0]
+    for row in csvReader:
+        mean[0] += int(row[colIndex])
+        mean[1] += 1
+    return (mean[0]/mean[1])
 
 
-# 3-  Create a function to read in the rainfall csv. Read each row and convert the average rainful to inches.  Write the city, the rain (in cm) and inches to a new file. (include column headings)
+# calling and printing the output of function
+print(meanCalc('birthwt.csv', 3))
+
 
 def createRainfallInIN(file):
     openedFile = open(file, 'r')
@@ -56,8 +55,8 @@ def createRainfallInIN(file):
         rainfallInCM = float(row[1])
         rainfallInIN = float(row[1])/2.54
         newRow = [city, rainfallInCM, rainfallInIN]
-        print(newRow)
         csvWriter.writerow(newRow)
 
 
 createRainfallInIN('rainfallInCM.csv')
+print("\nProblem 3: new file created")
