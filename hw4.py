27ba7e1cfa8af6f99dc1ad18f3832c404e1a5fd5
Pinny Geliebter <39172757+pgeliebter@users.noreply.github.com>
2022-09-22 23:00:52 -0400

    I am freakin awesome for getting this to work

--- hw4.py
+++ hw4.py
@@ -1,21 +1,6 @@
-# 1- Create a function that will return a list of n prime numbers (where n is the number of primes to calculate).  Add a function to write the list out to a file  You can use some of the earlier code used this semester as a starting point. For example, if we pass the function a parmeter of 5, it will return the first 5 prime numbers. Print out the list at the end.
-# 2- Create a function that receives a list of values and reverses the order of the list into a new list and returns the new list. Use loops to accomplish this and not built-in functions. for exampe,
+
 # 3- Write a function that will check if all items in a list are numeric. Hint: think about exception handlers. test with a list that has all numerics and a list that does not have all numerics.
 
-
-# def primeNumbers(numOfPrimes):
-# primes = []
-#  while len(primes) < numOfPrimes:
-#       # print(primes, numOfPrimes)
-#       startNum = 4
-#       for divisor in range(2, startNum):
-#           print(divisor)
-#           if (startNum % divisor) == 0:
-#               flag = True
-#       if flag != True:
-#           primes.append(startNum)
-#       startNum += 1
-#   return (primes)
 
 import csv
 
@@ -53,4 +38,21 @@ def primeNumbers(numOfPrimes):
     return primes
 
 
+print("Problem 1: new file created and here is list:")
 print(primeNumbers(20))
+
+print("\nProblem 2: ")
+
+
+def reverseList(list):
+    newList = []
+    index = len(list) - 1
+    while index >= 0:
+        newList.append(list[index])
+        index -= 1
+    return newList
+
+
+listToReverse = [10, 12, "hello",
+                 "this should now be in beginning"]
+print(reverseList(listToReverse))
