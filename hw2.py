6caf41f20449f973bfe57b1f8db10167239234d2
Pinny Geliebter <39172757+pgeliebter@users.noreply.github.com>
2022-09-22 21:05:32 -0400

    part 2 done

--- hw2.py
+++ hw2.py
@@ -1,11 +1,5 @@
-# Using the code from this weeks lecture as a guide, write code for the following questons:
 
-# 1- Download the attached file (hw2testfile.csv) into your python home directory. Look through the file (notice how one row of data contains non-numeric data) . Write code to total up the values of the file and print that final value. You will need to add an exception handler to handle the non-numeric data.   Keep the exception handler simple.  You can just print a line saying "A non numeric value has been ignored"
-
-# 2- Download the attached birthwt.csv file.  Determine how many smokers there are in the file.  This is all rows where smoke = 1.  Hints:  An if statement is needed in the loop. remember to convert the column to an integer before testing equivalence
-
-# NOTE: Please do not change the names of the input files. Also, please place your input files in your Python directory.  The path on your computer will not match mine.  Adhering to these will help speed up the grading process.
-
+import csv
 print("Problem 1: ")
 part1File = open('hw2testfile.csv', 'r')
 lines = part1File.readlines()
@@ -15,6 +9,22 @@ for line in lines:
         int(line)
         total += int(line)
     except:
-        print("A non number has been skipped")
+        print("A non number cell has been skipped")
         continue
 print(total)
+part1File.close()
+
+
+print("\nProblem 2: ")
+file = open('birthwt.csv', 'r')
+csvReader = csv.reader(file, delimiter=',')
+peopleArray = []
+smokeIndex = 0
+for i, key in enumerate(csvReader.__next__()):
+    if key == "smoke":
+        smokeIndex = i
+smokerTotal = 0
+for row in csvReader:
+    if int(row[smokeIndex]) == 1:
+        smokerTotal += 1
+print(smokerTotal)
