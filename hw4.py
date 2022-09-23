1265526e34c278995ed817995590592159002fd4
Pinny Geliebter <39172757+pgeliebter@users.noreply.github.com>
2022-09-22 23:12:06 -0400

    problem 3 added

--- hw4.py
+++ hw4.py
@@ -1,8 +1,5 @@
-
-# 3- Write a function that will check if all items in a list are numeric. Hint: think about exception handlers. test with a list that has all numerics and a list that does not have all numerics.
-
-
 import csv
+import numbers
 
 
 def checkIfPrime(num):
@@ -55,4 +52,23 @@ def reverseList(list):
 
 listToReverse = [10, 12, "hello",
                  "this should now be in beginning"]
+print(listToReverse)
 print(reverseList(listToReverse))
+
+print("\nProblem 3: ")
+
+
+def areAllYouGuysNumbers(data):
+
+    for item in data:
+        if (isinstance(item, numbers.Number)) != True:
+            return False
+    return True
+
+
+trueList = [12, 123, 123123, 123]
+falseList = [12, 123, 'hello', 123]
+print(
+    f'True List: {trueList}, function result: {areAllYouGuysNumbers(trueList)}')
+print(
+    f'False List: {falseList}, function result: {areAllYouGuysNumbers(falseList)}')
