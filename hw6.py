d15607211201abcda3bd77fd089fd3eba82cbf5d
Pinny Geliebter <39172757+pgeliebter@users.noreply.github.com>
2022-11-15 16:20:02 -0500

    hw7 done

--- hw6.py
+++ hw6.py
@@ -1,17 +1,17 @@
-import numpy as n
+import numpy as np
 import pandas as pd
 
-# boo = np.random.rand(5, 5)
-# cee = np.random.rand(5, 5)
+boo = np.random.rand(5, 5)
+cee = np.random.rand(5, 5)
 
-# add = boo+cee
-# multiply = boo*cee
-# print(add)
-# print(multiply)
+add = boo+cee
+multiply = boo*cee
+print(add)
+print(multiply)
 
-# flat = multiply.flatten()
-# calculations = {'std': np.std(flat), 'mean': np.mean(flat)}
-# print(calculations)
+flat = multiply.flatten()
+calculations = {'std': np.std(flat), 'mean': np.mean(flat)}
+print(calculations)
 
 data = {'name':
         ['Magnus Carlsen', 'Ding Liren', 'Ian Nepomniachtchi',
@@ -25,11 +25,5 @@ print(frame)
 newPlayer = {'name': ['Fabiano Caruana'], 'country': ['United States'],
              'score': [2766], 'rank':
              [6]}
-# print(pd.DataFrame(newPlayer))
 newFrame = pd.concat([frame, pd.DataFrame(newPlayer)], ignore_index=True)
 print(newFrame)
-
-# pd.concat([frame, ], ignore_index=True)
-
-# frame.concat(newPlayer)
-# print(frame)
